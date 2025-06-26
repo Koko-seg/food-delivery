c366f27f59e8955ffe7e9688c2ad88afb7402a96
Tsetsko <tseko0301@gmail.com>
2025-06-25 22:06:05 -0700

    added user model

---
+++ src/models/user.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  address: { type: String, required: true },
+  //   role: { type: String, required: true },
+  isVerified: { type: Boolean, required: true },
+  createdAt: { type: Date, default: Date.now() },
+  updatedAt: { type: Date, default: Date.now() },
+});
+
+const User = model("FoodCategory", userSchema);
+export default User;
