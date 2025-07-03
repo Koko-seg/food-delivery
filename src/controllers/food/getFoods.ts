e5a3cb85e192e3c8f7243ac17cf1383841bad991
Koko-seg <tseko0301@gmail.com>
2025-07-03 17:12:51 +0800

    getfoods

--- src/controllers/food/getFoods.ts
+++ src/controllers/food/getFoods.ts
@@ -3,11 +3,7 @@ import { Food } from "../../models/food";
 
 export const getFoodWithCategories = async (req: Request, res: Response) => {
   try {
-<<<<<<< HEAD
-    const foodWithCategories = await Food.aggregate([
-=======
     const foods = await Food.aggregate([
->>>>>>> 35575be5be313073d6851e448f3fd2019688dff9
       {
         $lookup: {
           from: "foodcategories",
@@ -16,13 +12,6 @@ export const getFoodWithCategories = async (req: Request, res: Response) => {
           as: "categoryDetails",
         },
       },
-<<<<<<< HEAD
-      { $unwind: "$categoryDetails" },
-      {
-        $group: {
-          _id: "$categoryDetails._id",
-          categoryName: { $first: "$categoryDetails.categoryName" },
-=======
       {
         $unwind: "$categoryDetails",
       },
@@ -32,7 +21,6 @@ export const getFoodWithCategories = async (req: Request, res: Response) => {
           categoryName: {
             $first: "$categoryDetails.categoryName",
           },
->>>>>>> 35575be5be313073d6851e448f3fd2019688dff9
           foods: {
             $push: {
               _id: "$_id",
@@ -42,14 +30,6 @@ export const getFoodWithCategories = async (req: Request, res: Response) => {
               ingredients: "$ingredients",
             },
           },
-<<<<<<< HEAD
-          count: { $sum: 1 },
-        },
-      },
-    ]);
-    // const foods = await Food.find().populate("category");
-    res.status(200).send({ success: true, foodWithCategories });
-=======
           count: {
             $sum: 1,
           },
@@ -57,7 +37,6 @@ export const getFoodWithCategories = async (req: Request, res: Response) => {
       },
     ]);
     res.status(200).send({ success: true, foods });
->>>>>>> 35575be5be313073d6851e448f3fd2019688dff9
   } catch (error) {
     res.status(400).send({ message: "api error", error });
   }
