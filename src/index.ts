d535ded5450e13fbb47247d9fdc3760c6288ce6c
Tsetsko <tseko0301@gmail.com>
2025-07-06 22:45:34 -0700

    jjj

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,13 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 
 import { connectDb } from "./database/database";
-import cors from "cors";
 
 import foodCategoryRouter from "./router/foodCategory.router";
 import foodRouter from "./router/food.router";
 import orderRouter from "./router/foodOrder.router";
 import userRouter from "./router/user.router";
 
-const port = 3000;
+const port = 3800;
 const app = express();
 
 app.use(express.json());
